2390107105533bc04efb696be99815dc51b2670b
Archivaldi <artur.markov1860@gmail.com>
2021-03-21 17:58:57 -0500

    login

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model { }
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -50,7 +55,7 @@ User.init(
             beforeCreate: async function (newUserData) {
                 return newUserData.password = await bcrypt.hash(newUserData.password, 10);
             },
-            beforeUpdate: async function (updatedUserData){
+            beforeUpdate: async function (updatedUserData) {
                 return updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
             }
         },
