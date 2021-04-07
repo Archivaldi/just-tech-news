926e37719ed6a29169cbb91498040dbeb95e5bcd
Archivaldi <artur.markov1860@gmail.com>
2021-04-07 03:30:53 +0300

    login logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -109,6 +109,16 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+    res.status(404).end();
+    }
+});
+
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
@@ -152,20 +162,6 @@ router.delete('/:id', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
-});
-
-router.post('/logout', (req, res) => {
-    console.log("You hit the logout route");
-    if (req.session.loggedIn) {
-        console.log('You were loggedIn')
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        console.log('You were not loggedIn')
-
-        res.status(404).end();
-    }
 });
 
 
